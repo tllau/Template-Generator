34ec88fc3e940ef1c2290625ea78d3b087e81c08
tllau <tllau0310@gmail.com>
2021-02-26 15:03:23 +0800

    Create "read_file" function and a sample text. Able to read a txt file and print in console.

--- template-generator.py
+++ template-generator.py
@@ -1,4 +1,9 @@
-
+def read_file(filename):
+    with open(filename, 'r') as f:
+        text = f.read()
+    print(text)
+        
 
 if __name__ == "__main__":
-    print("hello")
+    filename = "templates/test.txt"
+    read_file(filename)
