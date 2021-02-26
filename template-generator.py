4a4f4797c9dae7fb04e424c83795c3ca8ef6037d
tllau <tllau0310@gmail.com>
2021-02-26 15:56:25 +0800

    Create "user_form_test". Sucessfully ask user to input three items and display at the final output text.

--- template-generator.py
+++ template-generator.py
@@ -3,14 +3,22 @@ def read_file(filename):
         text = f.read()
     return text
         
-def fill_in_template(filename):
-    sample_item = {"date": "26FEB"}
-    text = read_file(filename)    
-    fill_text = text.replace("{%%date%%}", sample_item["date"])
-    return fill_text
+def fill_in_template(filename, filled_items):
+    text = read_file(filename)
+    for item in filled_items:
+        text = text.replace(f"<%%{item}%%>", filled_items[item])
+    return text
     
-
+def user_form():
+    items = ["name", "age", "hobby"]
+    filled_items = {}
+    for item in items:
+        filled_items[item] = input(f"What is your {item}? ")
+    return filled_items
+    
+    
 if __name__ == "__main__":
-    filename = "templates/fill_test.txt"
-    print(fill_in_template(filename))
+    filename = "templates/user_form_test.txt"
+    filled_items = user_form()
+    print(fill_in_template(filename, filled_items))
     
