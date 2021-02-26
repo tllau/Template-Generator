c46aada009329c721758322ad50650e2cd82dc09
tllau <tllau0310@gmail.com>
2021-02-26 15:31:05 +0800

    create fill_in_template to perform placeholder str replace

--- template-generator.py
+++ template-generator.py
@@ -1,9 +1,16 @@
 def read_file(filename):
     with open(filename, 'r') as f:
         text = f.read()
-    print(text)
+    return text
         
+def fill_in_template(filename):
+    sample_item = {"date": "26FEB"}
+    text = read_file(filename)    
+    fill_text = text.replace("{%%date%%}", sample_item["date"])
+    return fill_text
+    
 
 if __name__ == "__main__":
-    filename = "templates/test.txt"
-    read_file(filename)
+    filename = "templates/fill_test.txt"
+    print(fill_in_template(filename))
+    
