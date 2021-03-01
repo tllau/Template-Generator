ff131bf72de80daf4573ce4f374189ca8ce67471
Gary Lau <tllau0310@gmail.com>
2021-03-01 17:14:16 +0800

    use Session for passing template name

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from template_generator import Template
 
 app = Flask(__name__)
+app.secret_key = "b'\x0cZf\xcdg\xae\x884\xe5F\x08\xb62\x8b\n\xf4'"
+
 
 @app.route("/")
 def index():
@@ -9,17 +11,17 @@ def index():
 
 @app.route("/fill_in_template")
 def fill_in_template():
-    template_name = request.args.get("template")
-    test = Template(f"static/{template_name}.txt")
+    session["template"] = request.args.get("template")    
+    test = Template(f"static/{session['template']}.txt")
     text = test.get_text()
     items = test.get_items()
     return render_template("fill_in_template.html", text=text, items=items)
         
 @app.route("/result", methods = ["POST", "GET"])        
 def result():
-    test = Template("static/user_form_test.txt")
+    test = Template(f"static/{session['template']}.txt")
     filled_text = test.get_text()
-    if request.method == "POST":
+    if request.method == "POST":       
         # replace <%%item_name%%> with user input
         for item in test.get_items():
             filled_text = filled_text.replace(f"<%%{item}%%>", request.form[item])
@@ -28,5 +30,4 @@ def result():
         return redirect("/")
 
 if __name__ == "__main__":
-    test = Template("static/user_form_test.txt")
     app.run(debug=True)
